98305982d3891a86f32401ee1d0d185aaf2f70f1
Juheng Wu <cs15lwi23anb@ieng6-203.ucsd.edu>
2023-01-25 14:52:44 -0800

    add test

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,35 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 2, 1, 0 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 0,1,2,3 }, input1);
 	}
+@Test
+public void testReverseInPlace2() {
+    int[] input1 = { 1, 2, 3, 1, 2, 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3, 2, 1, 3, 2, 1 }, input1);
+}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 1 };
+    assertArrayEquals(new int[]{ 1 }, ArrayExamples.reversed(input1));
   }
+
+ @Test
+ public void testAveragewithoutlowest() {
+	 int [] input1 = { 1, 2 ,3 ,4}
+	 assertEquals)3.0, ArrayExamples.averageWithoutLowest(input1), .1);
+
+	}
+
+  @Test
+  public void testReversed1() {
+	  int[] input2 = { 1, 2 ,3};
+	  assertEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input2));
+
+	}
 }
